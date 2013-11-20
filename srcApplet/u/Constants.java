01d5e884d99ee102df1065a79e5d960396d59f30
Beatrice Popescu Braileanu <bpopescu@colibri.ll.iac.es>
2013-11-20 18:14:03 +0000

    modified server url

--- srcApplet/u/Constants.java
+++ srcApplet/u/Constants.java
@@ -2,7 +2,7 @@ package u;
 
 public class Constants {
 
-	public static String host = "sienai.ull.es";
+	public static String host = "siena-csl.isaatc.ull.es/sj-server";
 	public static String context = "sj";
 	
 	
